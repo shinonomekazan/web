4d4d7a9d4a26f45774455dd4224018a26893ad8c
Toyokazu Tsugehara <tsugehara@gmail.com>
2019-02-07 13:07:05 +0900

    とりあえずコンテンツ書いた

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// burgerを有効にするためのbulma公式サンプルより抜粋
+function activateBurgers() {
+	document.querySelectorAll(".burger").forEach(function(el) {
+		el.addEventListener("click", function() {
+			var target = el.dataset.target;
+			target = document.getElementById(target);
+			el.classList.toggle("is-active");
+			target.classList.toggle("is-active");
+		});
+	});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	activateBurgers();
+});
