846be954bb90e737796f508832eb8730a20c3865
tsugehara <tsugehara@gmail.com>
2021-02-01 16:53:57 +0900

    Prettier追加 (#36)
    * prettierの追加

--- start.mjs
+++ start.mjs
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
 		}
 		switch (req.url) {
 			case "/":
-			return "./html/index.html";
+				return "./html/index.html";
 			case "/index.html":
 			case "/work.html":
 			case "/about.html":
@@ -20,15 +20,15 @@ const server = http.createServer((req, res) => {
 			case "/logo.html":
 			case "/tech.html":
 			case "/jobs.html":
-			return "./html/" + req.url.substr(1);
+				return "./html/" + req.url.substr(1);
 			case "/css/main.css":
 			case "/js/index.js":
 			case "/favicon.ico":
 			case "/android-touch-icon.png":
 			case "/apple-touch-icon.png":
-			return "./" + req.url.substr(1);
+				return "./" + req.url.substr(1);
 			default:
-			return null;
+				return null;
 		}
 	}
 	const f = getPath();
@@ -47,36 +47,36 @@ const server = http.createServer((req, res) => {
 		switch (ext) {
 			case ".png":
 				res.writeHead(200, {
-					"Content-Type": "image/png"
+					"Content-Type": "image/png",
 				});
 				break;
 			case ".svg":
 				res.writeHead(200, {
-					"Content-Type": "image/svg+xml"
+					"Content-Type": "image/svg+xml",
 				});
 				break;
 			case ".ico":
 				res.writeHead(200, {
-					"Content-Type": "image/x-icon"
+					"Content-Type": "image/x-icon",
 				});
 				break;
 			case ".pdf":
 				res.writeHead(200, {
-					"Content-Type": "application/pdf"
+					"Content-Type": "application/pdf",
 				});
 				break;
 			case ".css":
 				res.writeHead(200, {
-					"Content-Type": "text/css"
+					"Content-Type": "text/css",
 				});
 				break;
 			case ".js":
 				res.writeHead(200, {
-					"Content-Type": "text/javascript"
+					"Content-Type": "text/javascript",
 				});
 			default:
 				res.writeHead(200, {
-					"Content-Type": "text/html"
+					"Content-Type": "text/html",
 				});
 		}
 		res.end(data);
